bb3a7dc044c49cac4a0045aadcfe4c8cf1ff11ae
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2023-05-30 16:00:28 -0300

    Correção Enum FormatoData

--- src/types/FormatoData.ts
+++ src/types/FormatoData.ts
@@ -1,5 +1,5 @@
 export enum FormatoData {
-    PADRAO = "dd/MM/yyyy",
-    DIA_SEMANA_DIA_MES_ANO = "EEEE, dd/MM/yyyy",
-    DIA_MES = "dd/MM",
+    PADRAO = "DD/MM/AAAA",
+    DIA_SEMANA_DIA_MES_ANO = "DIA_SEMANA, DD/MM/AAAA",
+    DIA_MES = "DD/MM",
 }
