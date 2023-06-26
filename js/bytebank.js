3678ac4d516f17a0edc76fa0bbef24f76a686a49
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2023-06-26 08:14:14 -0300

    Aula 01

--- js/bytebank.js
+++ js/bytebank.js
@@ -1,41 +1,37 @@
-let saldo = 3000;
-
-const elementoSaldo = document.querySelector(".saldo-valor .valor");
-elementoSaldo.textContent = saldo;
-
-const elementoFormulario = document.querySelector(".block-nova-transacao form");
-elementoFormulario.addEventListener("submit", function(event) {
+var saldo = 3000;
+var elementoSaldo = document.querySelector(".saldo-valor .valor");
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+var elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function (event) {
     event.preventDefault();
     if (!elementoFormulario.checkValidity()) {
         alert("Por favor, preencha todos os campos da transação!");
         return;
     }
-
-    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
-    const inputValor = elementoFormulario.querySelector("#valor");
-    const inputData = elementoFormulario.querySelector("#data");
-
-    let tipoTransacao = inputTipoTransacao.value;
-    let valor = inputValor.value;
-    let data = inputData.value;
-
+    var inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    var inputValor = elementoFormulario.querySelector("#valor");
+    var inputData = elementoFormulario.querySelector("#data");
+    var tipoTransacao = inputTipoTransacao.value;
+    var valor = inputValor.valueAsNumber;
+    var data = new Date(inputData.value);
     if (tipoTransacao == "Depósito") {
         saldo += valor;
-    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+    }
+    else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
         saldo -= valor;
-    } else {
+    }
+    else {
         alert("Tipo de Transação é inválido!");
         return;
     }
-
-    elementoSaldo.textContent = saldo;
-
-    const novaTransacao = {
+    elementoSaldo.textContent = saldo.toString();
+    var novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
     };
-
     console.log(novaTransacao);
     elementoFormulario.reset();
 });
